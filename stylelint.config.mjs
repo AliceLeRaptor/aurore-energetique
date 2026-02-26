6ea1d87ae83b971da5d9c7d4f7ca91407360ebe7
Alice Fauquet <alice.fauquet@radiofrance.com>
2026-02-26 15:24:16 +0100

    add style and framework plugin, create basic css files

---
+++ stylelint.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import("stylelint").Config} */
+export default {
+  extends: ["stylelint-config-standard"],
+  plugins: ["stylelint-no-unsupported-browser-features"],
+  rules: {
+    "selector-not-notation": "simple",
+    "plugin/no-unsupported-browser-features": [
+      true,
+      {
+        ignore: ["css-nesting"],
+        severity: "warning",
+      },
+    ],
+    "selector-class-pattern": [
+      "^([a-zA-Z][a-z0-9]*)(-[a-z0-9]+)*$",
+      {
+        message: (selector) =>
+          `Expected class selector "${selector}" to be kebab-case or capitalized kebab`,
+      },
+    ],
+  },
+};
