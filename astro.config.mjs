84eeb8b3f220e9fed00b2cc391a59c11e0d80f9f
Alice Fauquet <alice.fauquet@radiofrance.com>
2026-03-03 14:27:00 +0100

    add robots.txt and sitemap, add imgAlt to care entry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import preact from '@astrojs/preact';
+import preact from "@astrojs/preact";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+// TODO: changer l'url du site
 export default defineConfig({
-  integrations: [preact()]
+  site: "https://aurore-energetique.fr",
+  integrations: [
+    preact(),
+    sitemap({
+      filter: (page) => page !== "https://aurore-energetique.fr/admin/",
+    }),
+  ],
 });
