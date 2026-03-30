3331407ef427d8ddf8001af9c6e2ac7fa4e0536d
Alice Fauquet <alice.fauquet@radiofrance.com>
2026-03-30 17:29:35 +0200

    remove fake urls and add action for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,10 @@
 // @ts-check
 import { defineConfig, fontProviders } from "astro/config";
 
-import preact from "@astrojs/preact";
-
-import sitemap from "@astrojs/sitemap";
-
 // https://astro.build/config
-// TODO: changer l'url du site
 export default defineConfig({
-  site: "https://aurore-energetique.fr",
-  integrations: [
-    preact(),
-    sitemap({
-      filter: (page) => page !== "https://aurore-energetique.fr/admin/",
-    }),
-  ],
+  site: "https://AliceLeRaptor.github.io",
+  base: "/aurore-energetique",
   fonts: [
     {
       provider: fontProviders.local(),
